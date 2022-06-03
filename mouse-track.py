c1b28d00b1f1d0d852647074591855e398b02823
Zoe-Lackner <61874540+Zoe-Lackner@users.noreply.github.com>
2022-06-03 11:29:18 +0200

    mouse-track working

--- mouse-track.py
+++ mouse-track.py
@@ -1,3 +1,4 @@
+#pip install pygame
 import pygame
 from pygame.locals import *  #just so that some extra functions work
 pygame.init() #this turns pygame 'on'
@@ -11,7 +12,7 @@ while running:
     if event.type == pygame.QUIT:
         running = 0
     elif event.type == pygame.MOUSEMOTION:
-        print "mouse at (%d, %d)" % event.pos
+        print("mouse at (%d, %d)" % event.pos)
         if event.pos == (300,200):
             screen = pygame.display.set_mode((400, 500))
     screen.fill((0, 0, 0))
